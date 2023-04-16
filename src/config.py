9fd60f578a775a3484f4e583e70590174d8bd4b4
Ramazan2002 <ramazanui2002@gmail.com>
2023-04-17 02:28:45 +0300

    Create config file

---
+++ src/config.py
@@ -0,0 +1,16 @@
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    DB_USER: str
+    DB_USER_PASSWORD: str
+    DB_HOST: str
+    DB_NAME: str
+    DB_PORT: int
+    DB_URL: str
+
+    class Config:
+        env_file = "./.env"
+
+
+settings = Settings()
