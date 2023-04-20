c23297db5b9478a380805c17d938a5c9070382a1
Ramazan2002 <ramazanui2002@gmail.com>
2023-04-20 15:26:00 +0300

    Fix the problem

--- src/schemas/role.py
+++ src/schemas/role.py
@@ -14,10 +14,16 @@ class RoleSchemaUpdate(RoleSchemaBase):
     name: str | None = Field(min_length=2, max_length=20)
 
 
-class RoleSchema(BaseModel):
+class RoleUserSchema(BaseModel):
     id: int
     name: str
     description: str | None = None
+
+    class Config:
+        orm_mode = True
+
+
+class RoleSchema(RoleUserSchema):
     users: list["UserSchema"] | None = Field(
         include={"__all__": {"id", "username", "email", "fio"}}
     )
