4ecdd78aa78921b0c31352709f86eeed961074f0
Ramazan2002 <ramazanui2002@gmail.com>
2023-04-21 14:55:12 +0300

    Add path method get_user and refactor delete_user method

--- src/routers/user.py
+++ src/routers/user.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from ..schemas.user import UserSchemaCreate, UserSchemaUpdate, UserSchema
-from ..services.user import create, get, get_all, update
+from ..services.user import create, get_by_id, get_all, update, get_by_username, delete
 from ..db import get_db
 from fastapi_jwt_auth import AuthJWT
 
@@ -15,8 +15,19 @@ async def get_current_user(
 ):
     authorize.jwt_required()
     current_user = authorize.get_jwt_subject()
-    db_user = await get(db, current_user)
+    db_user = await get_by_username(db, current_user)
     return db_user
+
+
+@users_router.get("/{user_id}", response_model=UserSchema)
+async def get_user(
+    user_id: int, db: AsyncSession = Depends(get_db), authorize: AuthJWT = Depends()
+):
+    authorize.jwt_required()
+    user = await get_by_id(db, user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
 
 
 @users_router.get("", response_model=list[UserSchema])
@@ -55,7 +66,7 @@ async def update_user(
     if not any(new_user_data.values()):
         raise HTTPException(status_code=400)
 
-    existed_user = await get(db, username=username)
+    existed_user = await get_by_username(db, username=username)
 
     if not existed_user:
         raise HTTPException(status_code=400, detail="User not found")
@@ -68,12 +79,13 @@ async def delete_user(
     user_id: int, db: AsyncSession = Depends(get_db), authorize: AuthJWT = Depends()
 ):
     authorize.jwt_required()
-    username = authorize.get_jwt_subject()
     user_claims = authorize.get_raw_jwt()["user_claims"]
 
     if not user_claims["id"] == user_id:
         raise HTTPException(status_code=405)
 
-    existed_user = await get(db, username=username)
+    existed_user = await get_by_id(db, user_id)
     if not existed_user:
         raise HTTPException(status_code=400, detail="User not found")
+
+    return await delete(db, existed_user)
