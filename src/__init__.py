e254dee0f8368068d80a7a5e7b1b298a429e514d
ramazanix <ramazanui2002@gmail.com>
2023-04-25 00:06:28 +0300

    Add cors middleware

--- src/__init__.py
+++ src/__init__.py
@@ -23,11 +23,23 @@ def init_app(init_db=True):
     from .routers.summary import summaries_router
     from .handlers import auth_jwt_exception_handler
     from fastapi_jwt_auth.exceptions import AuthJWTException
+    from fastapi.middleware.cors import CORSMiddleware
+
+    origins = [
+        "http://localhost:3000",
+    ]
 
     server.include_router(auth_router)
     server.include_router(users_router)
     server.include_router(summaries_router)
     server.add_exception_handler(AuthJWTException, auth_jwt_exception_handler)
+    server.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
     return server
 
